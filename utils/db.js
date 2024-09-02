1682f72929200b610072b70518431f31ee8d176d
TevinDeja <teviowena@gmail.com>
2024-09-02 01:38:33 -0700

    Redis

---
+++ utils/db.js
@@ -0,0 +1,41 @@
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+  constructor() {
+    this.host = process.env.DB_HOST || 'localhost';
+    this.port = process.env.DB_PORT || 27017;
+    this.database = process.env.DB_DATABASE || 'files_manager';
+    this.client = new MongoClient(`mongodb://${this.host}:${this.port}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  }
+
+  async connect() {
+    await this.client.connect();
+    this.db = this.client.db(this.database);
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    const users = this.db.collection('users');
+    const count = await users.countDocuments();
+    return count;
+  }
+
+  async nbFiles() {
+    const files = this.db.collection('files');
+    const count = await files.countDocuments();
+    return count;
+  }
+
+  async close() {
+    await this.client.close();
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = { dbClient };
